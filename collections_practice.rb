178d8e184b7310897aeb5eb4fc016a3a80201529
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-31 23:40:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,20 +10,14 @@ end
 
 
 def sort_array_char_count(array)
-  
 array.sort! { |firstl, secondl| firstl.length <=> secondl.length }
-  
 end
 
 def swap_elements(array)
-  
-  
   array[1], array[2] = array[2], array[1]
   array
 end
 
 def reverse_array(array)
-  
   array.reverse
-  
 end
